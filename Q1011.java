d12afa20b8ccd4b4367911c36ac8757708bc88ef
eunho792 <67632047+eunho792@users.noreply.github.com>
2020-09-22 17:48:30 +0900

    Create Q1011.java

---
+++ Q1011.java
@@ -0,0 +1,17 @@
+import java.util.GregorianCalendar;
+import java.util.Random;
+import java.util.Scanner;
+import static java.lang.Math.*;
+class Main {
+	  public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("실수");
+		double x = sc.nextDouble();
+		
+		System.out.println("절대값:"+abs(x));
+		System.out.println("제곱근"+sqrt(x));
+		System.out.println("넓 이:"+PI*x*x);
+		 
+	  }
+	  
+	}
